e517607a5495aa22a5f7ad8ee98c162bdf33f754
xemdetia <xemdetia@808inorganic.com>
2017-03-05 00:12:00 -0500

    add get registry value function

--- local-windows-nt/local-windows-nt.el
+++ local-windows-nt/local-windows-nt.el
@@ -22,6 +22,33 @@ action to occur on start."
 	     (file-exists-p "~/.emacs.d/local/local-windows-nt/init_cmdproxy.exe.sh"))
 	(copy-file "~/.emacs.d/local/local-windows-nt/init_cmdproxy.exe.sh" "~/.emacs.d/")))
 
+(defun load-windows-nt--get-registry-value (path key)
+  "Using reg.exe get the registry value for key.
+The PATH includes the rootkey of HKLM HKCU and so on.  If no KEY
+is found, nil is returned.  Because this is tied to startup a
+failed key lookup writes to *Messages*"
+  (let ((inhibit-message t)
+	(exe (concat (getenv "windir") "\\system32\\reg.exe "))
+	(buffer (get-buffer-create "*local-reg*"))
+	(arg (concat "query "
+		     "\"" path "\" "
+		     "/v "
+		     key)))
+    ;; This is easiest to debug by using `with-current-buffer' and
+    ;; then erasing it with `erase-buffer' before the insert.
+    (with-current-buffer buffer
+      (erase-buffer)
+      (insert (shell-command-to-string
+	       (concat exe arg)))
+      (goto-char (point-min)) ; critical: leave point here for later regex
+      (flush-lines "^$")
+      (if (re-search-forward "^ERROR: The system was unable to find the specified registry key or value" nil t)
+	  (progn
+	    (message (concat "local-windows-nt: Could not find (" path " . " key ")"))
+	    nil)
+	(when (search-forward "REG_" nil nil 1) ; only take first occurance
+	  (nth 2 (split-string (thing-at-point 'line) "    " t "\r?\n")))))))
+
 ;;; Initialization Section:
 (load-windows-nt--install-shell-file)
 
