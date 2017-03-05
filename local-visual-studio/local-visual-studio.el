8cba313a6b4748eded13f12e3b493c5c24a0568e
xemdetia <xemdetia@808inorganic.com>
2017-03-05 01:05:15 -0500

    Extract some variables from vcvarsqueryregistry.bat

--- local-visual-studio/local-visual-studio.el
+++ local-visual-studio/local-visual-studio.el
@@ -6,10 +6,30 @@
 ;; clang mode and company-clang work properly.
 
 ;;; Configuration:
-(defvar load-windows-nt:visual-studio-version "14.0"
+;; vcvarsqueryregistry.bat
+(defvar local-visual-studio:framework-40-version "v4.0"
+  "Defaults to 'v4.0'.
+Maps to environment variable Framework40Version")
+
+(defvar local-visual-studio:visual-studio-version "14.0"
   "Determine what version of Visual Studio to extract from the registry.
 
-Defaults to '14.0' which is equivalent to Visual Studio 2015.")
+Defaults to '14.0' which is equivalent to Visual Studio
+2015. Maps to enviroment variable VisualStudioVersion after
+init.")
+
+(defvar local-visual-studio:windows-sdk-dir ""
+  "Maps to environment variable WindowsSdkDir.")
+
+(defvar local-visual-studio:windows-lib-path ""
+  "Maps to environment variable WindowsLibPath.")
+
+(defvar local-visual-studio:windows-sdk-version ""
+  "Maps to environment variable WindowsSDKVersion.")
+
+(defvar local-visual-studio:windows-sdk-lib-version "winv6.3\\"
+  "Maps to environment variable WindowsSDKLibVersion.
+Defaults to 'winv6.3\' which is default in Visual Studio 2015.")
 
 ;;; Code:
 
