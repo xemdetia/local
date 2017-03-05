03af15ee397ce8aa4c67d4485cd431a301a1dd41
xemdetia <xemdetia@808inorganic.com>
2017-03-05 02:02:50 -0500

    Add init fun for windows-sdk-version
    - default for windows-sdk-version is required to do the logic
      described in the batch file
    - The prefix-list comes from the batch file and we use it blindly

--- local-visual-studio/local-visual-studio.el
+++ local-visual-studio/local-visual-studio.el
@@ -5,6 +5,8 @@
 ;; SDK.  The primary goal is to provide enough information so that
 ;; clang mode and company-clang work properly.
 
+(require 'local-windows-nt)
+
 ;;; Configuration:
 ;; vcvarsqueryregistry.bat
 (defvar local-visual-studio:framework-40-version "v4.0"
@@ -24,14 +26,35 @@ init.")
 (defvar local-visual-studio:windows-lib-path ""
   "Maps to environment variable WindowsLibPath.")
 
-(defvar local-visual-studio:windows-sdk-version ""
-  "Maps to environment variable WindowsSDKVersion.")
+(defvar local-visual-studio:windows-sdk-version "10.0"
+  "Maps to environment variable WindowsSDKVersion.
+By default it is '10.0' to match Windows 10")
 
 (defvar local-visual-studio:windows-sdk-lib-version "winv6.3\\"
   "Maps to environment variable WindowsSDKLibVersion.
 Defaults to 'winv6.3\' which is default in Visual Studio 2015.")
 
 ;;; Code:
+(defun local-visual-studio--init-windows-sdk-dir ()
+  "Initialize `local-visual-studio:windows-sdk-dir'.  This is based on `local-visual-studio:windows-sdk-version'."
+  (let ((reg-path (concat "\\Microsoft\\Microsoft SDKs\\Windows\\v" ; registry key has 'v'
+			  local-visual-studio:windows-sdk-version))
+	; this list is a visual studio strategy of mitigating registry
+	; weirdness
+	(prefix-list '("HKLM\\SOFTWARE\\Wow6432Node"
+		       "HKCU\\SOFTWARE\\Wow6432Node"
+		       "HKLM\\SOFTWARE"
+		       "HKCU\\SOFTWARE"))
+	(seeking t))
+    (while (and (> (length prefix-list) 0)
+		seeking)
+      (when (setq local-visual-studio:windows-sdk-dir
+		  (load-windows-nt--get-registry-value
+		   (concat (pop prefix-list) reg-path)
+		   "InstallationFolder"))
+	(setq seeking nil)
+      )
+    (not seeking)))) ; not looking? found one
 
 (provide 'local-visual-studio)
 ;;; local-visual-studio.el ends here
